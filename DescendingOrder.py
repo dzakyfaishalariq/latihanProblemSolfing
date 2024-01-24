c7dd9a30080d97113b5605c8bc988b2b2adebbfd
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-01-24 21:42:19 +0700

    problem solfing

---
+++ DescendingOrder.py
@@ -0,0 +1,15 @@
+def descending_order(num):
+    list_num = []
+    num_bantu = num
+    num_baru = 0
+    while num_bantu != 0:
+        list_num.append(num_bantu%10)
+        num_bantu //= 10
+    list_num = sorted(list_num,reverse=True)
+    # print(list_num)
+    for i in range(len(list_num)):
+        # print(num_baru)
+        num_baru += list_num[i]*(10**(len(list_num)-1-i))
+    return num_baru
+        
+print(descending_order(15))
