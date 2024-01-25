ae402821b01a133a6cb13d7b4ab7ff32820ee939
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-01-25 15:45:24 +0700

    problem solfing

---
+++ Countofpositivessumofnegatives.py
@@ -0,0 +1,17 @@
+def count_positives_sum_negatives(arr):
+    count_positif = 0
+    sum_negatif = 0
+    if arr == []:
+        count_positif = 0
+        sum_negatif = 0
+    else: 
+        for i in arr:
+            if i > 0 :
+                count_positif += 1
+            elif i < 0:
+                sum_negatif += i
+            else:
+                continue
+    return [count_positif, sum_negatif]
+
+print(count_positives_sum_negatives([]))
