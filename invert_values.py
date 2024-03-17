a9991ba1b5a4488820b4e03511653be497e99242
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-03-17 11:01:51 +0700

    problem solving

---
+++ invert_values.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def invert(lst):
+    return -1*np.array(lst)
+
+print(invert([1,2,3,4,5]))
--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,14 @@
 def tow_sum(number, target):
-    return
+    hasil = None
+    for i in number:
+        for j in number:
+            if j == i:
+                continue
+            else:
+                if i + j == target:
+                    hasil = ()
+                else:
+                    hasil = False
+    return hasil    
 
 print(tow_sum([1,2,3], 4))
